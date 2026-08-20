2857a15a0bcded21b3ec69c48467595e64fb99d8
anjali-2511 <anjalimalharishinde2511@gmail.com>
2026-08-20 15:55:24 +0530

    String Practice Questions added

---
+++ String/Practice-Questions.py
@@ -0,0 +1,50 @@
+# Python Strings – Practice Set
+# This practice set is based on the topics we’ve covered so far:
+# Strings in Python
+# String Slicing and Indexing
+# String Methods and Functions
+# String Formatting and f-Strings
+# Strings (2 questions)
+# String Manipulation
+# Use these exercises to strengthen your understanding of strings and become confident in manipulating them.
+
+
+# 1. Basic String Operations
+# Create a string variable name with your full name. Print:
+# The first character
+# The last character
+# The length of the string
+# Concatenate two strings: "Hello" and "World" with a space in between.
+
+
+# 2. String Slicing and Indexing
+# Given text = "Python Programming", do the following:
+# Print the first 6 characters
+# Print the last 6 characters
+# Print every second character from the string
+# Reverse the string text using slicing.
+
+
+# 3. String Methods and Functions
+# Take the string "  i love python programming  " and:# Remove extra spaces from both ends
+# Convert it to title case
+# Count how many times "o" appears
+# Check if the string "123abc" is alphanumeric.
+
+
+# 4. String Formatting and f-Strings
+# Using format(), create a sentence:
+# "My name is John and I am 25 years old."
+# by passing "John" and 25 as variables.
+# Do the same using f-strings.
+
+
+# 5. String Manipulation Challenges
+# Given sentence = "Coding in Python is fun", replace "fun" with "awesome" and print it.
+# Find the index of the word "Python" in sentence.
+# Convert the entire sentence to uppercase and print it.
+
+
+# 6. Bonus Questions
+# Write a program that counts how many vowels are in a given string.
+# # Take a user input string and check if it is a palindrome (same forwards and backwards).
