b7ec5de62ca2c470972ed319debcaf55be11ea4d
anjali-2511 <anjalimalharishinde2511@gmail.com>
2026-08-20 15:05:12 +0530

    One More Quize added

--- Python Fundamentals/Practice-Questions.py
+++ Python Fundamentals/Practice-Questions.py
@@ -78,3 +78,31 @@ print("This is a tab →\t after tab.");
 num = int(input("Enter a Number:"));
 print(f"Square: {num ** 2}");
 print(f"Cube: {num  ** 3}");
+
+# Q9: Quick Quiz (True/False)
+# Mark each as True or False:
+
+# 1. Python code must always end with a semicolon ;
+False
+
+# 2. The # symbol is used for comments in Python
+True
+
+# 3. "123" and 123 are the same in Python
+False
+
+# 4. The * operator is used for multiplication
+True
+
+# 5. \n creates a new line
+True
+
+# 6. Variables in Python can start with numbers
+False
+
+# 7. int("10") + 5 gives 15
+True
+
+ 
+
+ 
