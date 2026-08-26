15cdb3e88534e6107afff6c5cfc4e3a608b88f23
anjali-2511 <anjalimalharishinde2511@gmail.com>
2026-08-26 11:32:57 +0530

    All practice questions completed

--- Control Flow and Loops/Practice-Questions.py
+++ Control Flow and Loops/Practice-Questions.py
@@ -98,19 +98,76 @@ for i in range(1, 5):
 # 333
 # 4444
 # 55555
+for i in range(1, 6):
+    print(str(i) * i)
+
  
 # 6. Use a for loop to print the following pattern:
 # 1111111111111111111111111111111
+for i in range(20):
+    print(1 , end="") 
+print()
 
 
 # 4. While Loops: 
-# 1. Print numbers from 1 to 10 using a while loop. 
-# 2. Print the multiplication table of a number  (entered by user) using a while loop
-# 3. Write a program that keeps asking the user to enter a password until they enter the correct one.
-# 4. Use a while loop to reverse a given number (e.g., 123 → 321).
+# 1. Print numbers from 1 to 10 using a while loop.
+n = 1
+while n < 11:
+    print(n)
+    n += 1; 
 
+# 2. Print the multiplication table of a number  (entered by user) using a while loop
+table = int(input("Enter a Table: "))
+t = 1
+while  t < 11:
+    print(t * table)
+    t += 1;
+
+# 3. Write a program that keeps asking the user to enter a password until they enter the correct one.
+password = "1234"
+p = input("Enter a password: ")
+while p != password:
+    print("Incorrect password, try again")
+    p = input("Enter a password: ")
+print("Correct Password")
+
+# 4. Use a while loop to reverse a given number (e.g., 123 → 321).
+no = 123
+reversed_no = 0
+while no > 0:
+    digit = no % 10
+    reversed_no = reversed_no * 10 + digit
+    no = no // 10
+print(reversed_no)
+    
 
 # 5. Break, Continue, and Pass Statements:
-# 1. Use a for loop to print numbers from 1 to 10, but stop the loop if the number is 7 (use break).
+# 1. Use a for loop to print numbers from 1 to 10, but stop the loop if the number is 7 
+# (use break).
+userno = int(input("Enter a number: "))
+for i in range(1, 11):
+    if i == 7:
+        break
+    print(i)
+
+# 2. Use a while loop to print numbers from 1 to 10
+n = 1
+while n < 11:
+    print(n)
+    n += 1; 
+
 # 2. Print numbers from 1 to 10, skipping the number 5 (use continue).
+n = 1
+while n < 11:
+    if i == 5:
+        continue
+    print(n)
+    n += 1; 
+
 # 3. Write a loop that goes through numbers 1 to 5, but does nothing for number 3 (use pass).
+n = 1
+while n < 6:
+    if i == 3:
+        pass
+    print(n)
+    n += 1;
